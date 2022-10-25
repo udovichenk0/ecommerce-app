f1a71f7ef0ee683c32710628e6cf591e2635e45a
Dennisss <denzeldenisq@gmail.com>
2022-10-25 21:21:54 +0300

    config updated

--- src/shared/api/api.ts
+++ src/shared/api/api.ts
@@ -8,32 +8,50 @@ export const featured = [
 	{
 		glasses: burnikk,
 		title: 'Balakubak',
-		subtitle: 'Sexbomb'
+		subtitle: 'Sexbomb',
+		id: 1,
+		sizes: [28, 26, 42],
+		price: 240,
+		description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat placeat similique dicta nulla praesentium deserunt. Corporis repellendus deleniti dolores eligendi.'
 	},
 	{
 		glasses: kibal,
 		title: 'Kibal Batal',
-		subtitle: 'Salt Maalat'
+		subtitle: 'Salt Maalat',
+		id:2,
+		price: 325,
+		sizes: [28, 26, 42],
+		description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat placeat similique dicta nulla praesentium deserunt. Corporis repellendus deleniti dolores eligendi.'
 	},
 	{
 		glasses: nice,
 		title: 'Very Nice',
-		subtitle: 'Salt maalat'
+		subtitle: 'Salt maalat',
+		id: 3,
+		price: 160,
+		sizes: [28, 26, 42],
+		description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat placeat similique dicta nulla praesentium deserunt. Corporis repellendus deleniti dolores eligendi.'
+
 	},
 	{
 		glasses: buldit,
 		title: 'Buldit',
-		subtitle: 'Salt Maalat'
+		subtitle: 'Salt Maalat',
+		id: 4,
+		price: 645,
+		sizes: [28, 42],
+		description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat placeat similique dicta nulla praesentium deserunt. Corporis repellendus deleniti dolores eligendi.'
+
 	},
 	{
 		glasses: balakubak,
 		title: 'Balakubak',
-		subtitle: 'Betsin Maalat'
-	},
-	{
-		glasses: kibal,
-		title: 'Balakubak',
-		subtitle: 'Sexbomb'
+		subtitle: 'Betsin Maalat',
+		id: 5,
+		price: 400,
+		sizes: [28],
+		description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat placeat similique dicta nulla praesentium deserunt. Corporis repellendus deleniti dolores eligendi.'
+
 	},
 ]
 
@@ -41,31 +59,70 @@ export const recommended = [
 	{
 		glasses: burnikk,
 		title: 'Burnikk',
-		subtitle: 'Sexbomb'
+		subtitle: 'Sexbomb',
+		id: 6,
+		price: 250,
+		sizes: [28, 26, 42],
+		description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat placeat similique dicta nulla praesentium deserunt. Corporis repellendus deleniti dolores eligendi.'
+
 	},
 	{
 		glasses: kibal,
 		title: 'Kibal Batal',
-		subtitle: 'Kibal Black'
+		subtitle: 'Kibal Black',
+		id: 7,
+		price: 550,
+		sizes: [42],
+		description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat placeat similique dicta nulla praesentium deserunt. Corporis repellendus deleniti dolores eligendi.'
+
 	},
 	{
 		glasses: nice,
 		title: 'Very Nice',
-		subtitle: 'Salt Maalat'
+		subtitle: 'Salt Maalat',
+		id: 3,
+		price: 300,
+		sizes: [26, 42],
+		description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat placeat similique dicta nulla praesentium deserunt. Corporis repellendus deleniti dolores eligendi.'
+
 	},
 	{
 		glasses: buldit,
 		title: 'Kulangot',
-		subtitle: 'Salt'
+		subtitle: 'Salt',
+		id: 8,
+		price: 299.99,
+		sizes: [28, 26, 42],
+		description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat placeat similique dicta nulla praesentium deserunt. Corporis repellendus deleniti dolores eligendi.'
+
 	},
 	{
 		glasses: sipon,
 		title: 'Sipon Malapot',
-		subtitle: 'Salt'
+		subtitle: 'Salt',
+		id: 9,
+		price: 249.50,
+		sizes: [28, 26, 42],
+		description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat placeat similique dicta nulla praesentium deserunt. Corporis repellendus deleniti dolores eligendi.'
+
 	},
 	{
 		glasses: balakubak,
 		title: 'Pitaklan',
-		subtitle: 'Black Kibal'
+		subtitle: 'Black Kibal',
+		id: 10,
+		price: 200,
+		sizes: [28, 42],
+		description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat placeat similique dicta nulla praesentium deserunt. Corporis repellendus deleniti dolores eligendi.'
 	},
 ]
+
+
+
+const getById = (id:number) => {
+	return recommended.find(el => id === el.id)
+}
+
+const sortByPrice = (min:number,max:number) => {
+	return recommended.filter(a => a.price >= min && a.price <= max)
+}
--- src/shared/ui/header/ui.tsx
+++ src/shared/ui/header/ui.tsx
@@ -15,7 +15,7 @@ export const Header = () => {
 					</div>
 				</Link>
 				<nav>
-					<ul className='flex items-center text-tr-dark font-medium'>
+					<ul className='flex items-center text-[#101010ba] font-medium'>
 						<li className='py-[10px] px-4 text-main-dark'><Link to={'/'}>Home</Link></li>
 						<li className='py-[10px] px-4'><Link to={'/'}>Shop</Link></li>
 						<li className='py-[10px] px-4'><Link to={'/'}>Featured</Link></li>
