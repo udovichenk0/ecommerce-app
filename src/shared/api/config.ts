667d492f6a12b19faf4b0c3f61228bb020a1be06
Dennisss <denzeldenisq@gmail.com>
2022-12-14 13:11:43 +0300

    test

--- src/shared/api/config.ts
+++ src/shared/api/config.ts
@@ -2,7 +2,7 @@ import { initializeApp } from "@firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  apiKey: "AIzaSyC_JUnLrfe3Q6-Lt4-kh7UiUv8HSGLEzrk",
   authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
   databaseURL: import.meta.env.VITE_FIREBASE_DB_URL,
   projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
